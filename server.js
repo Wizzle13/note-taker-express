5aa1f4e5734a93a6b657f24c91ae7b2e25274685
CBurton <burton.chris13@gmail.com>
2022-05-13 17:53:49 -0500

    created all html and api routes

--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const PORT = process.env.PORT || 3002; //sets the port for the site to 3002 unless defined by host
 const app = express();
 const htmlRoutes = require('./routes/htmlRoutes');
+const apiRoutes = require('./routes/apiRoutes');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 // routes
+app.use('/api/notes', apiRoutes);
 app.use('/notes', htmlRoutes);
 app.use('/', htmlRoutes);
 
