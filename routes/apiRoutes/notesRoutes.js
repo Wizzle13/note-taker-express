686435e02040d03c8b93434b4684629bd1e1cbc0
CBurton <burton.chris13@gmail.com>
2022-05-20 11:04:22 -0500

    updated created post route, does not work??

--- routes/apiRoutes/notesRoutes.js
+++ routes/apiRoutes/notesRoutes.js
@@ -2,9 +2,16 @@ const router = require('express').Router();
 
 const { notes } = require('../../db/db.json');
 
+// Gets all Notes
 router.get('/notes', (req,res) => {
     let results = notes;
     res.json(results);
+});
+
+// Add Note
+router.post('/notes', (req,res) => {
+    
+    res.json(notes);
 });
 
 module.exports = router;
